c507b0bed875ccfde0b79a69ffc70fa6c4eb3d56
A.J. Satcher <a.j.satcher@AJs-MacBook-Pro.local>
2017-03-13 10:49:32 -0400

    list dividers

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,50 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
-
+import _ from 'lodash';
 import { TeamHomePage } from '../pages';
 import { EliteApiService } from '../../providers/shared';
-
 @Component({
   selector: 'page-teams',
   templateUrl: 'teams.html'
 })
 export class TeamsPage {
-
+  private allTeams: any;
+  private allTeamDivisions: any;
   teams = [];
-  /*teams = [
-          { id: 1, name: 'HC Elite' },
-          { id: 2, name: 'Team Takeover' },
-          { id: 3, name: 'DC Thunder' }
-      ];*/
-
   constructor(private navCtrl: NavController,
               private navParams: NavParams,
               private eliteApi: EliteApiService,
               private loadingController: LoadingController) {}
-
   itemTapped($event, team){
     this.navCtrl.push(TeamHomePage, team);
   }
   /*itemTapped(){
     this.navCtrl.push(TeamHomePage);
   }*/
-
-
-  ionViewDidLoad() {
+  ionViewDidLoad(){
+    //console.log('**lodash debug', lodash, _);
     let selectedTourney = this.navParams.data;
-
     let loader = this.loadingController.create({
-      content: 'Getting Tournaments...'
+      content: 'Getting data...'
     });
-
     loader.present().then(() => {
-    this.eliteApi.getTournamentData(selectedTourney.id)
-      .subscribe(data => {
-        this.teams = data.teams;
+      this.eliteApi.getTournamentData(selectedTourney.id).subscribe(data => {
+        this.allTeams = data.teams;
+        this.allTeamDivisions =
+            _.chain(data.teams)
+            .groupBy('division')
+            .toPairs()
+            .map(item => _.zipObject(['divisionName', 'divisionTeams'], item))
+            .value();
+        this.teams = this.allTeamDivisions;
+        console.log('division teams', this.teams);
         loader.dismiss();
       });
     });
-    //console.log('ionViewDidLoad TeamsPage');
   }
-
 }
